5d42bbdfe5ffb816522c91174452a6df87da1fcc
Ngô͘ Hê-bí <shiami@users.noreply.github.com>
2024-10-22 10:38:13 +0800

    Khǹg chu-liāu.

--- assets/javascripts/viewer.js
+++ assets/javascripts/viewer.js
@@ -1,10 +1,10 @@
 (function () {
   // TODO edit
   const dataFolderPath = '/chheh';
-  const dataNamePrefix = '/TJTST1_';
-  const dataNameNumberCount = 3;
+  const dataNamePrefix = '/1931TOGCGK_Page_';
+  const dataNameNumberCount = 4;
   const dataNameSuffix = '.jpg';
-  const lastPageNumber = 12;
+  const lastPageNumber = 1237;
 
   let imgSources = [];
   const imgSourcesMin = 1;
